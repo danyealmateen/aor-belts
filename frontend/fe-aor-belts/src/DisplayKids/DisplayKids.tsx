209ba503ad17569ae773c203f179e5124ca73f7e
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-12-19 16:18:15 +0100

    added more components

--- frontend/fe-aor-belts/src/DisplayKids/DisplayKids.tsx
+++ frontend/fe-aor-belts/src/DisplayKids/DisplayKids.tsx
@@ -8,39 +8,39 @@ function DisplayData() {
   const [filterStudents, setFilterStudents] = useState<Student[]>([]);
   const [data, setData] = useState<Student[] | null>(null);
 
-  async function updateData(updatedStudent: Student) {
-    setData((prevData) => {
-      if (prevData) {
-        const newData = prevData.map((s) =>
-          s._id === updatedStudent._id ? updatedStudent : s
-        );
-        return newData;
-      }
-      return prevData;
-    });
+  // async function updateData(updatedStudent: Student) {
+  //   setData((prevData) => {
+  //     if (prevData) {
+  //       const newData = prevData.map((s) =>
+  //         s._id === updatedStudent._id ? updatedStudent : s
+  //       );
+  //       return newData;
+  //     }
+  //     return prevData;
+  //   });
 
-    try {
-      const response = await fetch(
-        `https://aor-belts-main.onrender.com/api/barn/${updatedStudent._id}`,
-        {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(updatedStudent),
-        }
-      );
+  //   try {
+  //     const response = await fetch(
+  //       `https://aor-belts-main.onrender.com/api/barn/${updatedStudent._id}`,
+  //       {
+  //         method: 'PUT',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //         body: JSON.stringify(updatedStudent),
+  //       }
+  //     );
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
 
-      const responseData = await response.json();
-      console.log('Data successfully updated:', responseData);
-    } catch (error) {
-      console.error('Error updating student:', error);
-    }
-  }
+  //     const responseData = await response.json();
+  //     console.log('Data successfully updated:', responseData);
+  //   } catch (error) {
+  //     console.error('Error updating student:', error);
+  //   }
+  // }
 
   async function getData() {
     try {
--- frontend/fe-aor-belts/src/DisplayKids/UpdateKids.tsx
+++ frontend/fe-aor-belts/src/DisplayKids/UpdateKids.tsx
@@ -1,4 +1,58 @@
 const UpdateKids = () => {
+
+    async function updateData(updatedStudent: Student) {
+        setData((prevData) => {
+            if (prevData) {
+                const newData = prevData.map((s) =>
+                    s._id === updatedStudent._id ? updatedStudent : s
+                );
+                return newData;
+            }
+            return prevData;
+        });
+
+        try {
+            const response = await fetch(
+                `https://aor-belts-main.onrender.com/api/barn/${updatedStudent._id}`,
+                {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(updatedStudent),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const responseData = await response.json();
+            console.log('Data successfully updated:', responseData);
+        } catch (error) {
+            console.error('Error updating student:', error);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <>
             <h1>update-kids-comp</h1>
