cdc4989975f0cbe57bd89215c089faa649d4495c
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-12-22 11:15:01 +0100

    login for dashboard done

--- frontend/fe-aor-belts/src/Dashboard/Dashboard.tsx
+++ frontend/fe-aor-belts/src/Dashboard/Dashboard.tsx
@@ -15,6 +15,29 @@ const Dashboard = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const handleLogin = async (e: any) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`https://aor-belts-main.onrender.com/api/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+
+                },
+                body: JSON.stringify({ username, password })
+            }
+            );
+
+            if (response.ok) {
+                console.log('You are logged in!');
+            } else {
+                console.log('Username or password wrong!');
+            }
+
+        } catch (e) {
+            console.log('Couldnt login', e);
+        }
+    };
 
     return (
         <>
@@ -39,30 +62,25 @@ const Dashboard = () => {
                     <Modal.Title>Logga in</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form.Label htmlFor="username">Användarnamn</Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="username"
-                        onChange={(e) => {
-                            setUsername(e.target.value);
-                            console.log(username);
-                        }}
-                    />
-                    <Form.Label htmlFor="password">Lösenord</Form.Label>
-                    <Form.Control
-                        type="password"
-                        id="password"
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                            console.log(password);
-                        }}
-                    />
+                    <form onSubmit={handleLogin}>
+                        <Form.Label htmlFor="username">Användarnamn</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="username"
+                            onChange={(e) => setUsername(e.target.value)} />
+                        <Form.Label htmlFor="password">Lösenord</Form.Label>
+                        <Form.Control
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Avbryt
                     </Button>
-                    <Button variant="primary">Logga in</Button>
+
+                    <Button type='submit' variant="primary" onClick={handleLogin}>Logga in</Button>
                 </Modal.Footer>
             </Modal>
         </>
