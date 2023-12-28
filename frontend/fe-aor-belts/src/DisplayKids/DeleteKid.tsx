d2566f118b2267fddc61355bc5b44188b037aa5a
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-12-28 18:04:26 +0100

    delete function added

--- frontend/fe-aor-belts/src/DisplayKids/DeleteKid.tsx
+++ frontend/fe-aor-belts/src/DisplayKids/DeleteKid.tsx
@@ -1,25 +1,29 @@
 import { useContext } from "react";
 import { DataContext } from "./DataContext";
+import { Student } from "../Interfaces";
 
 const DeleteKid = ({ id }: any) => {
     const { data, setData } = useContext(DataContext);
     console.log(data);
 
     const handleDelete = async () => {
+        setData((prevData) => {
+            if (!prevData) return null;
+            const newData = prevData.filter((student: Student) => student._id !== id);
+            return newData;
+        });
+
+
         try {
             const response = await fetch(`https://aor-belts-main.onrender.com/api/barn/${id}`, {
                 method: 'DELETE'
             });
 
             if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
+                throw new Error(`Network response was not ok`);
             }
 
-            if (data) {
-                console.log(data);
-                setData(data.filter(student => student._id !== id));
-            }
-
+            console.log("Barnet har tagits bort");
 
         } catch (error) {
             console.log("Sorry cant delete the kid", error);
