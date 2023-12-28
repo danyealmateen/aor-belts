4d8b564e42b280d67e1230d8c59d0dff803c577e
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-12-28 17:52:53 +0100

    fetch function added

--- frontend/fe-aor-belts/src/DisplayKids/DeleteKid.tsx
+++ frontend/fe-aor-belts/src/DisplayKids/DeleteKid.tsx
@@ -3,10 +3,22 @@ import { DataContext } from "./DataContext";
 
 const DeleteKid = ({ id }: any) => {
     const { data, setData } = useContext(DataContext);
+    console.log(data);
 
     const handleDelete = async () => {
         try {
-            const response = await fetch(`https://aor-belts-main.onrender.com/api/barn/${id}`);
+            const response = await fetch(`https://aor-belts-main.onrender.com/api/barn/${id}`, {
+                method: 'DELETE'
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+
+            if (data) {
+                console.log(data);
+                setData(data.filter(student => student._id !== id));
+            }
 
 
         } catch (error) {
