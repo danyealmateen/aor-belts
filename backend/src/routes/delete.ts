2d89a831d169f7fd4a6ee69307ee14b5be1e6544
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-12-28 14:52:28 +0100

    adding a delete route

---
+++ backend/src/routes/delete.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { Barn } from "../schema/schema";
+
+const router = express.Router();
+
+router.delete("/deletekid", async (req,res) => {
+    try {
+        const deleteStudent = await 
+    }
+})
